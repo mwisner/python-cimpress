194609a03fe090af65cbbf087d54886fb383bf99
Matt Wisner <matt@detroitweb.company>
2017-08-02 16:41:31 -0400

    Raise errors on token gen

--- cimpress/authentication.py
+++ cimpress/authentication.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+from .errors import CimpressError
 
 def get_api_token(client_id, username, password):
     headers = {'Content-Type': 'application/json'}
@@ -16,5 +16,8 @@ def get_api_token(client_id, username, password):
     response = requests.post("https://cimpress.auth0.com/oauth/ro",
                              data=json.dumps(payload),
                              headers=headers)
+
+    if response.status_code != 200:
+        raise CimpressError(response.json().get('error_description'))
 
     return response.json().get('id_token', None)

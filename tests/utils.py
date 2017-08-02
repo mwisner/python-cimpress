92448e3267ea3e05b82333768d2838d37e287969
Matt Wisner <matt@detroitweb.company>
2017-08-02 16:27:39 -0400

    Adding exception handling on test util

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,7 @@
 import os
 from cimpress.authentication import get_api_token
 from cimpress.client import Client
-
+from cimpress.errors import CimpressError
 
 def get_client_with_token():
     client_id = os.environ.get('CIMPRESS_CLIENT_ID')
@@ -9,5 +9,8 @@ def get_client_with_token():
     password = os.environ.get('CIMPRESS_PASSWORD')
 
     token = get_api_token(client_id, username, password)
+
+    if not token:
+        raise CimpressError("Unable to generate token")
 
     return Client(token, 'sandbox')

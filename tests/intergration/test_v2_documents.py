0a9875ff84ba0d21d6336c5c07e01c391d818f8b
Matt Wisner <matt@detroitweb.company>
2017-08-02 18:26:17 -0400

    Adding successful doc creation test

--- tests/intergration/test_v2_documents.py
+++ tests/intergration/test_v2_documents.py
@@ -25,33 +25,4 @@ class ProductsTest(unittest.TestCase):
                                                    scale='stretchToFit',
                                                    method='urls')
 
-        from pprint import pprint
-        pprint(response)
-
-        #assert len(response)
-
-    @istest
-    def it_lists_product_prices(self):
-        response = self.client.v1_products.list_all_prices()
-        assert len(response)
-
-    @istest
-    def it_gets_product(self):
-        products = self.client.v1_products.list_all()
-        assert len(products)
-        product = products[0]
-        product = self.client.v1_products.get_product(product.get('Sku'))
-        from pprint import pprint
-        pprint(product)
-        assert product.get('Prices')
-
-    @istest
-    def it_gets_product_surfaces(self):
-        products = self.client.v1_products.list_all()
-        assert len(products)
-
-        product = self.client.v1_products.get_product_surfaces(products[0].get('Sku'))
-
-        assert product.get('Surfaces')
-
-
+        assert 'DocumentReferenceUrl' in response
